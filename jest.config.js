813743ab30aa835cdc799c82eb07ada7948b8dc6
Samir <bsam@bk.ru>
2024-07-09 01:08:54 +0300

    add tests environment

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+export default {
+    testEnvironment: "jsdom",
+    transform: {
+        "^.+\\.tsx?$": "ts-jest",
+    },
+
+    moduleNameMapper: {
+        "\\.(css|less|sass|scss)$": "identity-obj-proxy",
+        "^.+\\.svg$": "jest-transformer-svg",
+        "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+            "<rootDir>/mocks/fileMock.js",
+    },
+
+    setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+};
---
+++ src/entities/Todo/ui/TodoItem/TodoItem.test.tsx
@@ -0,0 +1,5 @@
+describe("", () => {
+    test("", () => {
+        expect(3).toBe(3);
+    });
+});
