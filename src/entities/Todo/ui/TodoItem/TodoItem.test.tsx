a2c22b7baf1913d210cf01e6dbc3a20aac262cdb
Samir <bsam@bk.ru>
2024-07-09 13:33:53 +0300

    add tests environment for react

--- src/entities/Todo/ui/TodoItem/TodoItem.test.tsx
+++ src/entities/Todo/ui/TodoItem/TodoItem.test.tsx
@@ -1,5 +1,15 @@
-describe("", () => {
-    test("", () => {
-        expect(3).toBe(3);
+import { render, screen } from "@testing-library/react";
+import { TodoItem } from "./TodoItem";
+
+describe("TodoItem", () => {
+    test("test if there is image inside button if todo is completed", () => {
+        render(
+            <TodoItem
+                todo={{ completed: true, value: "Test todo", id: 1 }}
+                onClick={() => {}}
+            />
+        );
+
+        expect(screen.getByRole("img")).toBeInTheDocument();
     });
 });
